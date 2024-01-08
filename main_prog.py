23e37a101e2adb8c119211bd9d1f3f1f7cf9f757
Yarox_ZHBR <yarox.k@yandex.ru>
2024-01-08 20:26:42 +0300

    отрисовка уровня

--- main_prog.py
+++ main_prog.py
@@ -19,6 +19,39 @@ def load_image(name, colorkey=None):
     return image
 
 
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+def load_level(filename):
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            # elif level[y][x] == '@':
+                # Tile('empty', x, y)
+                # new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
+
  # Место под классы
 
 
@@ -34,10 +67,13 @@ if __name__ == '__main__':
     player_group = pygame.sprite.Group()
     enemies_group = pygame.sprite.Group()
     tile_images = {
-        #
+        'wall': load_image('floor.png'),
+        'empty': load_image('wall.png')
     }
-    player_image = load_image('')
+    # player_image = load_image('')
     tile_width = tile_height = 50
+    player, level_x, level_y = generate_level(load_level('data/levels.txt'))
+    level_map = load_level('data/levels.txt')
     # Место под программу
     while running:
         for event in pygame.event.get():
@@ -45,7 +81,9 @@ if __name__ == '__main__':
                 running = False
 
         screen.fill('white')
-        # Место под прорисовку
+        all_sprites.draw(screen)
+        tiles_group.draw(screen)
+        player_group.draw(screen)
         clock.tick(fps)
         pygame.display.flip()
     pygame.quit()
